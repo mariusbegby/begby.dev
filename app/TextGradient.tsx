2aa455d177cd00d9f966bdf5342d5ef882ee9b74
Marius Begby <mariusbegby@gmail.com>
2024-08-06 21:18:14 +0200

    Changes

--- app/TextGradient.tsx
+++ app/TextGradient.tsx
@@ -1,19 +1,20 @@
 type TextGradientProps = {
-    from?: string;
-    to?: string;
-    via?: string;
-    text: string;
+	from?: string;
+	to?: string;
+	via?: string;
+	text: string;
 };
 
 export default function TextGradient(props: TextGradientProps) {
-    const from = props.from || 'from-blue-400';
-    const to = props.to || 'to-rose-400';
-    const via = props.via || 'via-purple-400';
+	const from = props.from || "from-blue-400";
+	const to = props.to || "to-sky-400";
+	const via = props.via || "via-indigo-400";
 
-    return (
-        <span
-            className={`animate-text-gradient bg-white bg-gradient-to-r bg-clip-text text-transparent ${from} ${via} ${to}`}>
-            {props.text}
-        </span>
-    );
+	return (
+		<span
+			className={`animate-text-gradient bg-white bg-gradient-to-r bg-clip-text text-transparent ${from} ${via} ${to}`}
+		>
+			{props.text}
+		</span>
+	);
 }
